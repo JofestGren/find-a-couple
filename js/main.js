f76e0b916bdbfed9df4da44a15d11a8a677b0f70
Legenda374 <okssartur@gmail.com>
2017-12-17 03:02:01 +0200

    Game field generation system

--- js/main.js
+++ js/main.js
@@ -1,26 +1,45 @@
-window.onload = () => {
-    let valueInput = v => document.getElementById(v);
+$(document).ready(function(){
 
-    var modal = valueInput('HelloModal'),
-    startGame = valueInput('startGameBtn'),
-    startGameBtn = valueInput('startGameBtn');
+	let valueInput = v => document.getElementById(v);
 
-    startGame.onclick = function() {
-        modal.style.display = "none";
+	function add(count) {
+		var index, valueIndex; 
+		var mixArray = [];
+		count = count/2;
+		for (var i = 1; i <= count; i++) {
+			mixArray.push(i);
+			mixArray.push(i);
+		}
+		for (var i = 0; i < mixArray.length; i++) {
+			index = Math.floor(Math.random()*i);
+			valueIndex = mixArray[index];
+			mixArray[index] = mixArray[i];
+			mixArray[i] = valueIndex;
+		}
+		return mixArray;
+	}
+
+	var modal = valueInput('HelloModal'),
+	startGame = valueInput('startGameBtn'),
+	startGameBtn = valueInput('startGameBtn');
+
+	function createGame() {
+		var category = valueInput('category').value,
+		sizeGame = valueInput('sizeGame').value;
+		console.log(category, sizeGame);
+        var game_array = add(sizeGame); //перемешиваем массив (картинки)
+        for (var i = 0; i < sizeGame; i++) {
+        	$("<div/>", {
+        		"class": "num" + game_array[i],
+        		"data-state": '0'
+        	}).appendTo(".game");
+        }
+        if(sizeGame == 4) $(".game").css('width','204px');
+         if(sizeGame == 12 || sizeGame == 16) $(".game").css('width','408px');
+         if(sizeGame == 20) $(".game").css('width','510px');
+         if(sizeGame == 30 || sizeGame == 36) $(".game").css('width','612px');
+      modal.style.display = "none";
     }
-
-    function createGame() {
-        var category = valueInput('category').value,
-        sizeGame = valueInput('sizeGame').value;
-        console.log(category, sizeGame);
-    }
-
     startGameBtn.onclick = createGame;
 
-    // window.onclick = function(event) {
-    //     if (event.target == modal) {
-    //         modal.style.display = "none";
-    //     }
-    // }
-
-}
+});
