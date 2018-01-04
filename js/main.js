0d5fb18f0a9c95753b51b95cf16552edcce5baa5
Jofest Gren <jofest.g.70@gmail.com>
2018-01-04 23:50:15 +0200

    Code Refactoring

--- js/main.js
+++ js/main.js
@@ -1,65 +1,51 @@
-$(document).ready(function(){
-
-	$('.select--category').click(function () {
-		var menu = $('.list--category');
-		var listItem = $('.list--category .modal-select__item')
-		$(this).toggleClass('active');
-		if (menu.is(':visible')) {
-			menu.slideUp();
-		} else {
-			menu.slideDown();
-		}
-	});
-
-	$('.select--sizegame').click(function () {
-		var menu = $('.list--sizegame');
-		$(this).toggleClass('active');
-		if (menu.is(':visible')) {
-			menu.slideUp();
-		} else {
-			menu.slideDown();
-		}});
+window.onload = () => {
 
 	let valueInput = v => document.getElementById(v);
-	var modal = valueInput('HelloModal'),
-	container = valueInput('containerSection'),
-	startGameBtn = valueInput('startGameBtn'),
-	answerBtn = valueInput('answerBtn'),
-	reGameBtn = valueInput('reGameBtn'),
-	resultNewGameBtn = valueInput('resultNewGameBtn'),
-	exitBtn = valueInput('exitBtn');
-	var game_flag = null;
 
-	let category = "Animals";
-	let sizeGame = 4;
+	let selectList = (classSelect, classMenu) => {
+		$(classSelect).click(function () {
+			let menu = $(classMenu);
+			$(this).toggleClass('active');
+			(menu.is(':visible')) ? menu.slideUp() : menu.slideDown();
+		});
+	}
+
+	selectList('.select--category','.list--category');
+	selectList('.select--sizegame','.list--sizegame');
+
+	let modal = valueInput('HelloModal'),
+		container = valueInput('containerSection'),
+		startGameBtn = valueInput('startGameBtn'),
+		answerBtn = valueInput('answerBtn'),
+		reGameBtn = valueInput('reGameBtn'),
+		resultNewGameBtn = valueInput('resultNewGameBtn'),
+		exitBtn = valueInput('exitBtn'),
+		game_flag = null;
+
+	let category = "Animals",
+		sizeGame = 4;
 
 	let checkElementCategory = (idList, nameSelect, idTitle, idDesc, nameDescTitle) => {
-		let list = document.querySelectorAll(`${idList} .modal-select__item`);
-		let title = valueInput(`${idTitle}`);
-		let desc = valueInput(`${idDesc}`);
-		let descTitleField = document.querySelector(`.${nameDescTitle}`);
+		let list = document.querySelectorAll(`${idList} .modal-select__item`),
+			title = valueInput(`${idTitle}`),
+			desc = valueInput(`${idDesc}`),
+			descTitleField = document.querySelector(`.${nameDescTitle}`);
+
 		for (let i = 0; i < list.length; i++) {
+
 			list[i].addEventListener('click', () => {
 				nameSelect = list[i].innerText;
 				title.innerText = nameSelect;
 				console.log(category, sizeGame, descTitleField);
-				if (desc.innerText !== title.innerText) {
-					descTitleField.style.display = 'block';
-				} else {
-					descTitleField.style.display = 'none';
-				}
+				(desc.innerText !== title.innerText) ? descTitleField.style.display = 'block' :
+				descTitleField.style.display = 'none';
 				console.log(nameSelect);
-
 			});
 		}
 	}
 
 	category = checkElementCategory('#category', category, 'titleCategory', 'descCategory', 'select-titles--category');
 	sizeGame = checkElementCategory('#sizeGame', sizeGame, 'titleSizeGame', 'descSizeGame', 'select-titles--sizeGame');
-
-	function nameSelect(name) {
-		return name;
-	};
 
 	let resultWindow = valueInput('resultWindow');
 
@@ -84,19 +70,22 @@ $(document).ready(function(){
 	}
 
 	function add(count) {
-		var index, valueIndex;
-		var mixArray = [];
+		let index, valueIndex;
+		let mixArray = [];
 		count = count/2;
+
 		for (var i = 1; i <= count; i++) {
 			mixArray.push(i);
 			mixArray.push(i);
 		}
+
 		for (var i = 0; i < mixArray.length; i++) {
 			index = Math.floor(Math.random()*i);
 			valueIndex = mixArray[index];
 			mixArray[index] = mixArray[i];
 			mixArray[i] = valueIndex;
 		}
+
 		return mixArray;
 	}
 
@@ -135,18 +124,19 @@ $(document).ready(function(){
     	}
 
     	let category = valueInput('titleCategory').innerText;
-    	let strSizeGame = valueInput('titleSizeGame').innerText;
-    	let sizeGame = Number(strSizeGame[0]) * Number(strSizeGame[2]);
+		(category === 'Category') ? category = 'Animals' : category = category;
+    	let strSizeGame = valueInput('titleSizeGame').innerText,
+    		sizeGame = Number(strSizeGame[0]) * Number(strSizeGame[2]);
     	console.log(category, sizeGame);
-    	var sizeGameTemp = sizeGame/2;
- 		var last_img; //Последняя показанная картинка
- 		var img_der = './image/'; //Путь к папке с картинками
- 		var count_click = 0; //Кол-во кликов
- 		var click_flag = 0;
-        var game_array = add(sizeGame); //перемешиваем массив (картинки)
-        var localStorageName = "bestTime";
-        var game_time = new Date();
-        var startDate = new Date();
+    	let sizeGameTemp = sizeGame/2,
+ 			last_img, //Последняя показанная картинка
+ 		 	img_der = './image/', //Путь к папке с картинками
+ 			count_click = 0, //Кол-во кликов
+ 			click_flag = 0,
+        	game_array = add(sizeGame), //перемешиваем массив (картинки)
+        	localStorageName = "bestTime",
+        	game_time = new Date(),
+        	startDate = new Date();
 
         for (var i = 0; i < sizeGame; i++) {
         	$("<div/>", {
@@ -157,6 +147,7 @@ $(document).ready(function(){
 
         img_der += category + '/';
         localStorageName += sizeGame;
+
         if(sizeGame == 4) $(".game").css('width','220px');
         if(sizeGame == 12 || sizeGame == 16) $(".game").css('width','440px');
         if(sizeGame == 20) $(".game").css('width','550px');
@@ -172,48 +163,48 @@ $(document).ready(function(){
         	game_flag = 0;
         };
 
-        reGameBtn.onclick = function() { location.reload() };
+        reGameBtn.onclick = () => location.reload();
         resultNewGameBtn.onclick = () => location.reload();
         exitBtn.onclick = () => window.close();
+
         document.timeForm.bestTime.value = localStorage.getItem(localStorageName);
 
 		$('.game div').click(function() { //Клик на игровом поле
 			game_flag = 1;
 			startTIME();
 
- 		if($(this).data('state') == 0 && click_flag == 0) { //Если ячейка закрыта
+	 		if($(this).data('state') == 0 && click_flag == 0) { //Если ячейка закрыта
 
- 			if(count_click == 0) { //Если первый клик по закрытому полю
- 				count_click++;
- 				last_img = $(this).attr('class');
- 				$(this).data('state',1).attr('data-state',1).css('backgroundImage', 'url(' + img_der + last_img + '.jpg)');
- 			} else {
+	 			if(count_click == 0) { //Если первый клик по закрытому полю
+	 				count_click++;
+	 				last_img = $(this).attr('class');
+	 				$(this).data('state',1).attr('data-state',1).css('backgroundImage', 'url(' + img_der + last_img + '.jpg)');
+	 			} else {
 
- 				//Если картинки совпадают
- 				if(last_img == $(this).attr('class')) {
- 					$('.' + last_img).data('state',2).attr('data-state',2).css('backgroundImage', 'url(' + img_der + last_img + '.jpg)');
- 					sizeGameTemp--;
- 				} else {
- 					$(this).data('state', 1).attr('data-state',1).css('backgroundImage', 'url(' + img_der + $(this).attr('class') + '.jpg)');
- 					click_flag = 1;
+	 				//Если картинки совпадают
+	 				if(last_img == $(this).attr('class')) {
+	 					$('.' + last_img).data('state',2).attr('data-state',2).css('backgroundImage', 'url(' + img_der + last_img + '.jpg)');
+	 					sizeGameTemp--;
+	 				} else {
+	 					$(this).data('state', 1).attr('data-state',1).css('backgroundImage', 'url(' + img_der + $(this).attr('class') + '.jpg)');
+	 					click_flag = 1;
+	 					setTimeout(clearImg, 700);
+	 				}
 
- 					setTimeout(clearImg, 700);
- 				}
+	 				count_click = 0;
+	 			}
+	 		}
 
- 				count_click = 0;
- 			}
- 		}
+	 		if (sizeGameTemp == 0) {
+	 			game_flag = 0;
 
- 		if (sizeGameTemp == 0) {
- 			game_flag = 0;
-
- 			if (game_time < (localStorage.getItem(localStorageName)) || localStorage.getItem(localStorageName) == undefined) {
- 				localStorage.setItem(localStorageName, game_time);
- 				resultCreate(game_time, 'best');
- 			}
- 			else
- 				resultCreate(game_time, 'time');
- 		}
- 	});
+	 			if (game_time < (localStorage.getItem(localStorageName)) || localStorage.getItem(localStorageName) == undefined) {
+	 				localStorage.setItem(localStorageName, game_time);
+	 				resultCreate(game_time, 'best');
+	 			}
+	 			else
+	 				resultCreate(game_time, 'time');
+	 		}
+ 		});
 	});
-});
+};
